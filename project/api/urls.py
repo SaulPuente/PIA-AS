d96731eee324bd4c37e24ec126c69405d06e3c12
“SaulPuente” <saul@wirebit.com>
2023-05-26 10:34:37 -0600

    B

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 #now import the views.py file into this code
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns=[
-    path('etiqueta/',views.test.as_view()),
+    path('etiqueta/',views.test.as_view(), name="tag_maker"),
 ]
